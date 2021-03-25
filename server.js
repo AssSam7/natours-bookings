887e39b5ddad0231b7a053161793298b921cb7da
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2021-03-25 22:42:48 +0530

    Adding the ESLint + Prettier Configurations

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const dotenv = require("dotenv");
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
 console.log(app.get("env"));
 
 const port = process.env.PORT || 3000;
